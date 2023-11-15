937835a193287ec77e512b33b7b04bd81dab4728
phonil <phonil@naver.com>
2023-11-16 00:50:34 +0900

    setting spring boot

---
+++ src/main/java/com/kkeujeok/KkeujeokApplication.java
@@ -0,0 +1,13 @@
+package com.kkeujeok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkeujeokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KkeujeokApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kkeujeok/KkeujeokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkeujeok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkeujeokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
