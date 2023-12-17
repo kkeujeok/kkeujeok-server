e7247d6b5f913decdbd12080ea8586ebda27fc4a
yuraup <o721@naver.com>
2023-12-18 03:00:33 +0900

    feat: CORS Config 추가

--- src/main/java/com/kkeujeok/global/config/CorsConfig.java
+++ src/main/java/com/kkeujeok/global/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.kkeujeok.global.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
